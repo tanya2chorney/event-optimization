a1a64a5b5da0ecad3284ab1615efc8c28b658713
tanya2chorney <tanyachorney5@gmail.com>
2024-10-02 20:00:39 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@
 const btnContainer = document.querySelector(".js-buttons");
 btnContainer.addEventListener("click", onButtonsContainerClick);
 
-const selectedBtns = [];
+// const selectedBtns = [];
+const selectedBtns = new Set();
 
 function onButtonsContainerClick(event) {
   if (event.target.nodeName !== "BUTTON") {
@@ -21,9 +22,17 @@ function onButtonsContainerClick(event) {
 
   //   event.target.classList.add("btn-active");
   const btn = event.target;
+  const btnName = btn.dataset.value;
+  const isActive = btn.classList.contains("btn-active");
+  if (isActive) {
+    selectedBtns.delete(btnName);
+  } else {
+    selectedBtns.add(btnName);
+  }
+
   btn.classList.toggle("btn-active");
 
   //   selectedBtns.push.event.target.dataset.value;
-  selectedBtns.push(event.target.dataset.value);
+  // selectedBtns.push(event.target.dataset.value);
   console.log(selectedBtns);
 }
